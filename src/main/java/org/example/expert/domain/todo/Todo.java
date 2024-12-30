e75e6354f53660efa38cf8e5ee97740c79329a11
roqkfchqh <plmokn48@daum.net>
2024-12-30 15:14:48 +0900

    fix: entity 양방향 관계를 제거했습니다

--- src/main/java/org/example/expert/domain/todo/Todo.java
+++ src/main/java/org/example/expert/domain/todo/Todo.java
@@ -29,18 +29,11 @@ public class Todo extends Timestamped {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
-    private List<Comment> comments = new ArrayList<>();
-
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
-    private List<Manager> managers = new ArrayList<>();
-
     public Todo(String title, String contents, String weather, User user) {
         this.title = title;
         this.contents = contents;
         this.weather = weather;
         this.user = user;
-        this.managers.add(new Manager(user, this));
     }
 
     public void update(String title, String contents) {
